c3fdec54b8f3f1dd4b2fd1d78a36b7f009f247eb
SWHL <liekkaskono@163.com>
2022-11-30 11:30:29 +0800

    Add actions to auto update.

--- ML.md
+++
@@ -1,5 +0,0 @@
-# 机器学习赛事经验贴
-> 从他人比赛经验中，总是可以学到很多东西
----
-#### [【2022-06-08】天池算法大赛项目：基于大规模日志的故障诊断亚军方案！](https://mp.weixin.qq.com/s/GwrcPwTNHTXb46ek0-rWKA)
-#### [【2022-02-08】kaggle项目：基于随机森林模型的心脏病患者预测分类！](https://mp.weixin.qq.com/s/ylREDKB-HFo0b2mxphXd_g)
--- scripts/spider_wechat_account.py
+++ scripts/spider_wechat_account.py
@@ -12,37 +12,23 @@ import requests
 from fake_useragent import UserAgent
 
 
-def read_txt(txt_path: str) -> list:
-    with open(txt_path, 'r', encoding='utf-8') as f:
-        data = list(map(lambda x: x.rstrip('\n'), f))
-    return data
-
-
-def write_txt(save_path: str, content: list, mode='w'):
-    if isinstance(content, str):
-        content = [content]
-    with open(save_path, mode, encoding='utf-8') as f:
-        for value in content:
-            f.write(f'{value}\n')
-
-
-def is_contain_str(src_text: Union[str, List],
-                   given_str_list: Union[str, List],) -> bool:
-    """src_text中是否包含given_str_list中任意一个字符
-
-    Args:
-        src_text (str or list):
-        given_str_list (str or list):
-
-    Returns:
-        bool:
-    """
-    return any(i in src_text for i in given_str_list)
-
-
 class SpiderArticleWeChat(object):
-    def __init__(self, fake_id, cookie, token) -> None:
+    def __init__(self, cookie, token) -> None:
         self.URL = "https://mp.weixin.qq.com/cgi-bin/appmsg"
+        self.others_key_words = ['赛题总结', '竞赛总结', '比赛总结', '图数据',
+                                 '风险趋势预测', '异常行为分析', '关联融合计算', '容量预测']
+        self.nlp_key_words = ['NLP', '情感识别', 'BERT', '问题匹配']
+
+        self.md_dict = {
+            0: '../NLP.md',
+            1: '../Others.md',
+        }
+
+        self.account_fakeid = {
+            'Coggle数据科学': 'MzIwNDA5NDYzNA==',
+            '一碗数据汤': 'MzI5ODQxMTk5MQ=='
+        }
+
         self.headers = {
             "Cookie": cookie,
             "User-Agent": str(str(UserAgent().random))
@@ -51,72 +37,128 @@ class SpiderArticleWeChat(object):
             "action": "list_ex",
             "begin": "0",
             "count": "5",
-            "fakeid": fake_id,
+            "fakeid": self.account_fakeid['Coggle数据科学'],
             "type": "9",
             "token": token,
             "lang": "zh_CN",
             "f": "json",
             "ajax": "1"
         }
-        self.key_word_list = ['赛题总结', '竞赛总结']
 
-    def __call__(self, begin: str = '0', md_path: str = '../Others.md'):
+    def __call__(self, begin: str = '0',):
         self.params["begin"] = str(begin)
         cur_date = datetime.strftime(datetime.now(), '%Y-%m-%d')
-        time.sleep(random.randint(1, 10))
 
-        first, second = self.get_already_data(md_path)
+        is_valid_article = False
+        for one_account, fake_id in self.account_fakeid.items():
+            self._random_sleep()
 
+            self.params['fakeid'] = fake_id
+            msg = self._get_url_response()
+
+            if "app_msg_list" in msg:
+                for item in msg["app_msg_list"]:
+                    article_date = self._get_time(item['create_time'])
+                    title, link = item.get('title'), item.get('link')
+
+                    # 根据title来决定写入哪个md
+                    idx = self._which_data(title)
+                    if idx is None:
+                        continue
+
+                    # 读取指定md文件的已有数据
+                    md_path = self.md_dict[idx]
+                    already_data = self.read_txt(md_path)
+
+                    # 按 --- 来切分为两部分
+                    splid_idx = already_data.index('---')
+                    head = already_data[:splid_idx+1]
+                    left = already_data[splid_idx+1:]
+
+                    # 当前抓取到的文章没有出现在已有列表中
+                    if not self.is_appear(title, left):
+                        competition_info = f'#### [【{cur_date}】{title}]({link})'
+
+                        head.append(competition_info)
+                        self.write_txt(md_path, head + left)
+                        is_valid_article = True
+
+        if is_valid_article:
+            return True
+        return False
+
+    def _get_url_response(self,):
         resp = requests.get(self.URL,
                             headers=self.headers,
                             params=self.params,
                             verify=False)
-
         msg = resp.json()
-        self.final_result = []
-        if "app_msg_list" in msg:
-            for item in msg["app_msg_list"]:
-                create_time = datetime.fromtimestamp(float(item['create_time']))
-                article_date = datetime.strftime(create_time, '%Y-%m-%d')
+        return msg
 
-                title = item['title']
-                link = item['link']
-                if cur_date == article_date \
-                        and self.have_article(title) \
-                        and not is_contain_str(second, title):
-                    self.final_result.append(f'#### [【{cur_date}】{title}]({link})')
-        self.write_to_md(md_path, first, second)
+    def _random_sleep(self, sleep_second: int = 10):
+        time.sleep(random.randint(1, sleep_second))
 
-    def have_article(self, cur_title: str) -> bool:
-        for one_key in self.key_word_list:
-            if one_key in cur_title:
-                return True
-        return False
+    def _get_time(self, seconds_str):
+        create_time = datetime.fromtimestamp(float(seconds_str))
+        article_date = datetime.strftime(create_time, '%Y-%m-%d')
+        return article_date
 
-    def get_already_data(self, md_path):
-        already_data = read_txt(md_path)
+    def _which_data(self, title: str) -> int:
+        if self.is_contain_str(title, self.others_key_words):
+            return 1
+        elif self.is_contain_str(title, self.nlp_key_words):
+            return 0
+        else:
+            return None
+
+    def read_md_data_split(self, md_path):
+        already_data = self.read_txt(md_path)
         idx = already_data.index('---')
         first, second = already_data[:idx+1], already_data[idx+1:]
         return first, second
 
-    def write_to_md(self, md_path, first, second):
-        if self.final_result:
-            for v in self.final_result:
-                first.append(v)
-            new_data_list = first + second
-            write_txt(md_path, new_data_list)
+    @staticmethod
+    def read_txt(txt_path: str) -> list:
+        with open(txt_path, 'r', encoding='utf-8') as f:
+            data = list(map(lambda x: x.rstrip('\n'), f))
+        return data
+
+    @staticmethod
+    def write_txt(save_path: str, content: list, mode='w'):
+        if isinstance(content, str):
+            content = [content]
+        with open(save_path, mode, encoding='utf-8') as f:
+            for value in content:
+                f.write(f'{value}\n')
+
+    @staticmethod
+    def is_contain_str(src_text: Union[str, List],
+                       given_str_list: Union[str, List],) -> bool:
+        for one_value in given_str_list:
+            if src_text.__contains__(one_value):
+                return True
+        return False
+
+    @staticmethod
+    def is_appear(title, left):
+        for one in left:
+            if title in one:
+                return True
+        return False
 
 
 def main(args):
-    spider = SpiderArticleWeChat(args.fake_id, args.cookie, args.token)
-    spider()
+    spider = SpiderArticleWeChat(args.cookie, args.token)
+    res = spider()
+    if res:
+        print(1)
+    else:
+        print(0)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fake_id', type=str, default='MzIwNDA5NDYzNA==')
     parser.add_argument('--cookie', type=str, required=True)
     parser.add_argument('--token', type=int, required=True)
     args = parser.parse_args()
-
     main(args)

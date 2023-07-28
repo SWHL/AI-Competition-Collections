3fe2e1fa11b7b194fce29872dfa55b8b1835a202
SWHL <liekkaskono@163.com>
2023-07-28 21:57:47 +0800

    Update files

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,85 +7,90 @@ from pathlib import Path
 import yaml
 
 
-class ConvertMDToHugo():
+class ConvertMDToHugo:
     def __init__(self):
         self.root_dir = Path(__file__).resolve().parent.parent
-        self.save_dir = self.root_dir / 'content'
+        self.save_dir = self.root_dir / "content"
 
-        self.cur_date = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        self.cur_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
 
         cur_dir = Path(__file__).resolve().parent
-        config = self.read_yaml(cur_dir / 'convert.yaml')
+        config = self.read_yaml(cur_dir / "convert.yaml")
 
-        global_config = config.get('Global')
-        self.author = global_config.get('Author')
-        self.author_email = global_config.get('AuthorEmail')
-        self.children = global_config.get('Children')
+        global_config = config.get("Global")
+        self.author = global_config.get("Author")
+        self.author_email = global_config.get("AuthorEmail")
+        self.children = global_config.get("Children")
 
-        self.home_config = config.get('Home')
-        self.cv_config = config.get('CV')
+        self.home_config = config.get("Home")
+        self.cv_config = config.get("CV")
         self.type_list = [
-            config.get('NLP'),
-            config.get('Others'),
-            config.get('Speech'),
-            config.get('Interview')
+            config.get("NLP"),
+            config.get("Others"),
+            config.get("Speech"),
+            config.get("Interview"),
         ]
 
-    def __call__(self,):
+    def __call__(
+        self,
+    ):
         self.convert_cv()
         for one_type in self.type_list:
             self.convert_one_type(one_type)
         self.create_home_index_md()
 
     def convert_one_type(self, config):
-        title = config.get('title')
-        menu_title = config.get('menuTitle')
-        weight = config.get('weight')
-        badge_list = config.get('badge_list', None)
+        title = config.get("title")
+        menu_title = config.get("menuTitle")
+        weight = config.get("weight")
+        badge_list = config.get("badge_list", None)
 
         md_content = [
-            '---',
+            "---",
             f'title: "{title}"',
             f'menuTitle: "{menu_title}"',
-            f'date: {self.cur_date}',
-            'draft: false',
-            f'weight: {weight}',
+            f"date: {self.cur_date}",
+            "draft: false",
+            f"weight: {weight}",
             'LastModifierDisplayName: "SWHL"',
             'LastModifierEmail: "liekkaskono@163.com"',
-            '---',
-            ' ',
-            '> 从他人比赛经验中，总是可以学到很多东西',
-            ' ',
+            "---",
+            " ",
+            "> 从他人比赛经验中，总是可以学到很多东西",
+            " ",
         ]
 
         if badge_list:
             for badge in badge_list:
                 badge_shortcut = f'{{{{% badge style="info" icon=" " title=" " %}}}}{badge}{{{{% /badge %}}}}'
                 md_content.append(badge_shortcut)
-            md_content.append(' ')
-        md_content.extend(['---', ' '])
+            md_content.append(" ")
+        md_content.extend(["---", " "])
 
-        ori_md_path = self.root_dir / f'{menu_title}.md'
+        ori_md_path = self.root_dir / f"{menu_title}.md"
         ori_md_data = self.read_txt(ori_md_path)
 
-        short_line_idx = ori_md_data.index('---')
-        ori_md_data = ori_md_data[short_line_idx + 1:]
-        ori_md_data = [v.replace('###', '######') if v == '### 友情链接' else v
-                       for v in ori_md_data]
+        short_line_idx = ori_md_data.index("---")
+        ori_md_data = ori_md_data[short_line_idx + 1 :]
+        ori_md_data = [
+            v.replace("###", "######") if v == "### 友情链接" else v for v in ori_md_data
+        ]
 
         save_md_dir = self.save_dir / menu_title
         self.mkdir(save_md_dir)
-        save_md_path = save_md_dir / '_index.md'
+        save_md_path = save_md_dir / "_index.md"
 
         md_content.extend(ori_md_data)
         self.write_txt(save_md_path, md_content)
 
-    def convert_cv(self,):
-        cur_cv_dir = self.root_dir / 'CV'
-        save_cv_dir = self.save_dir / 'CV'
+    def convert_cv(
+        self,
+    ):
+        cur_cv_dir = self.root_dir / "CV"
+        save_cv_dir = self.save_dir / "CV"
         self.mkdir(save_cv_dir)
 
-        cv_weight_dict = self.cv_config.get('cv_weight_dict')
+        cv_weight_dict = self.cv_config.get("cv_weight_dict")
 
         cv_list = list(cur_cv_dir.iterdir())
         for cv_path in cv_list:
@@ -97,55 +102,55 @@ class ConvertMDToHugo():
 
             # 定义前缀
             prefix_list = [
-                '---',
+                "---",
                 f'title: "{md_name}"',
-                f'date: {self.cur_date}',
-                'draft: false',
-                f'weight: {weight}',
+                f"date: {self.cur_date}",
+                "draft: false",
+                f"weight: {weight}",
                 f'LastModifierDisplayName: "{self.author}"',
                 f'LastModifierEmail: "{self.author_email}"',
-                '---',
-                ' '
+                "---",
+                " ",
             ]
 
             prefix_list.extend(md_content)
-            save_path = save_cv_dir / f'{md_name}.md'
+            save_path = save_cv_dir / f"{md_name}.md"
             self.write_txt(save_path, prefix_list)
 
-        self.create_cv_index_md(save_cv_dir / '_index.md')
+        self.create_cv_index_md(save_cv_dir / "_index.md")
 
     def create_cv_index_md(self, save_path: str):
-        title = self.cv_config.get('title')
-        weight = self.cv_config.get('weight')
+        title = self.cv_config.get("title")
+        weight = self.cv_config.get("weight")
 
         prefix_content = [
-            '---',
+            "---",
             f'title: "{title}"',
             'menuTitle: "CV"',
-            f'weight: {weight}',
-            '---',
-            ' ',
-            self.children
+            f"weight: {weight}",
+            "---",
+            " ",
+            self.children,
         ]
         self.write_txt(str(save_path), prefix_content)
 
     def create_home_index_md(self):
-        archetype = self.home_config.get('archetype')
-        title = self.home_config.get('title')
-        start_key = self.home_config.get('start_key')
-        end_key = self.home_config.get('end_key')
+        archetype = self.home_config.get("archetype")
+        title = self.home_config.get("title")
+        start_key = self.home_config.get("start_key")
+        end_key = self.home_config.get("end_key")
 
         prefix_content = [
-            '---',
+            "---",
             f'archetype: "{archetype}"',
             f'title: "{title}"',
-            '---',
-            ' ',
+            "---",
+            " ",
             self.children,
-            ' '
+            " ",
         ]
 
-        readme_path = self.root_dir / 'README.md'
+        readme_path = self.root_dir / "README.md"
         ori_readme = self.read_txt(readme_path)
 
         # 找到竞赛公众号的索引
@@ -156,12 +161,13 @@ class ConvertMDToHugo():
 
             if end_key in v:
                 end_idx = i
-        home_content = ori_readme[start_idx:end_idx + 1]
-        home_content = [v.replace('###', '######') if '###' in v else v
-                        for v in home_content]
+        home_content = ori_readme[start_idx : end_idx + 1]
+        home_content = [
+            v.replace("###", "######") if "###" in v else v for v in home_content
+        ]
         prefix_content.extend(home_content)
 
-        save_path = self.save_dir / '_index.md'
+        save_path = self.save_dir / "_index.md"
         self.write_txt(save_path, prefix_content)
 
     @staticmethod
@@ -173,28 +179,28 @@ class ConvertMDToHugo():
         if not isinstance(txt_path, str):
             txt_path = str(txt_path)
 
-        with open(txt_path, 'r', encoding='utf-8') as f:
-            data = list(map(lambda x: x.rstrip('\n'), f))
+        with open(txt_path, "r", encoding="utf-8") as f:
+            data = list(map(lambda x: x.rstrip("\n"), f))
         return data
 
     @staticmethod
-    def write_txt(save_path: str, content: list, mode='w'):
+    def write_txt(save_path: str, content: list, mode="w"):
         if not isinstance(save_path, str):
             save_path = str(save_path)
 
         if isinstance(content, str):
             content = [content]
-        with open(save_path, mode, encoding='utf-8') as f:
+        with open(save_path, mode, encoding="utf-8") as f:
             for value in content:
-                f.write(f'{value}\n')
+                f.write(f"{value}\n")
 
     @staticmethod
     def read_yaml(yaml_path):
-        with open(yaml_path, 'rb') as f:
+        with open(yaml_path, "rb") as f:
             data = yaml.load(f, Loader=yaml.Loader)
         return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     converter = ConvertMDToHugo()
     converter()
--- scripts/spider_wechat_account.py
+++ scripts/spider_wechat_account.py
@@ -9,60 +9,58 @@ from datetime import datetime
 from typing import List, Union
 
 import requests
-from fake_useragent import UserAgent
 import yaml
+from fake_useragent import UserAgent
 
 
 class SpiderArticleWeChat(object):
     def __init__(self, cookie, token) -> None:
-        self.args = self.read_yaml('args.yaml')
+        self.args = self.read_yaml("args.yaml")
 
-        self.URL = self.args['URL']
-        self.nlp_keywords = self.args['Type']['NLP']['keywords']
-        self.nlp_invaild_keywords = self.args['Type']['NLP']['invalid_keywords']
+        self.URL = self.args["URL"]
+        self.nlp_keywords = self.args["Type"]["NLP"]["keywords"]
+        self.nlp_invaild_keywords = self.args["Type"]["NLP"]["invalid_keywords"]
 
-        self.others_keywords = self.args['Type']['Others']['keywords']
-        self.others_invalid_keywords = self.args['Type']['Others']['invalid_keywords']
-        self.interview_keywords = self.args['Type']['Interview']['keywords']
+        self.others_keywords = self.args["Type"]["Others"]["keywords"]
+        self.others_invalid_keywords = self.args["Type"]["Others"]["invalid_keywords"]
+        self.interview_keywords = self.args["Type"]["Interview"]["keywords"]
 
         self.md_dict = {
-            0: self.args['Type']['NLP']['md_path'],
-            1: self.args['Type']['Others']['md_path'],
-            2: self.args['Type']['Interview']['md_path']
+            0: self.args["Type"]["NLP"]["md_path"],
+            1: self.args["Type"]["Others"]["md_path"],
+            2: self.args["Type"]["Interview"]["md_path"],
         }
 
-        self.account_fakeid = self.args['FakeID']
+        self.account_fakeid = self.args["FakeID"]
 
-        self.headers = {
-            "Cookie": cookie,
-            "User-Agent": str(str(UserAgent().random))
-        }
+        self.headers = {"Cookie": cookie, "User-Agent": str(str(UserAgent().random))}
         self.params = {
             "action": "list_ex",
             "begin": "0",
             "count": "5",
-            "fakeid": 'MzIwNDA5NDYzNA==',
+            "fakeid": "MzIwNDA5NDYzNA==",
             "type": "9",
             "token": token,
             "lang": "zh_CN",
             "f": "json",
-            "ajax": "1"
+            "ajax": "1",
         }
-        self.cur_date = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        self.cur_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
         self.have_valid_nums = 0
 
     def __call__(self, spider_pages=3):
         begin_list = [str(i) for i in range(0, spider_pages * 5, 5)]
 
         for one_account, fake_id in self.account_fakeid.items():
-            self.params['fakeid'] = fake_id
+            self.params["fakeid"] = fake_id
 
             spider_content_list = []
             for begin in begin_list:
                 self._random_sleep()
 
-                self.params['begin'] = begin
+                self.params["begin"] = begin
                 msg_list = self._get_url_response()
+
                 if msg_list:
                     spider_content_list.extend(msg_list)
             self.process_content(spider_content_list)
@@ -85,29 +83,34 @@ class SpiderArticleWeChat(object):
             already_data = self.read_txt(md_path)
 
             # 按 --- 来切分为两部分
-            splid_idx = already_data.index('---')
-            head = already_data[:splid_idx+1]
-            left = already_data[splid_idx+1:]
+            splid_idx = already_data.index("---")
+            head = already_data[: splid_idx + 1]
+            left = already_data[splid_idx + 1 :]
 
             # 当前抓取到的文章没有出现在已有列表中
             if not self.is_appear(title, left):
-                competition_info = f'- [【{self.cur_date}】{title}]({link})'
+                competition_info = f"- [【{self.cur_date}】{title}]({link})"
 
                 head.append(competition_info)
                 self.write_txt(md_path, head + left)
                 self.have_valid_nums += 1
 
-    def _get_url_response(self,):
-        resp = requests.get(self.URL,
-                            headers=self.headers,
-                            params=self.params,
-                            verify=False)
+    def _get_url_response(
+        self,
+    ):
+        resp = requests.get(
+            self.URL,
+            headers=self.headers,
+            params=self.params,
+            verify=False,
+            timeout=60,
+        )
         msg = resp.json()
-        if 'app_msg_list' in msg:
+        if "app_msg_list" in msg:
             res = []
-            for item in msg['app_msg_list']:
-                create_time = self._get_time(item['create_time'])
-                title, link = item.get('title'), item.get('link')
+            for item in msg["app_msg_list"]:
+                create_time = self._get_time(item["create_time"])
+                title, link = item.get("title"), item.get("link")
                 res.append([create_time, title, link])
             return res
         return None
@@ -117,16 +120,18 @@ class SpiderArticleWeChat(object):
 
     def _get_time(self, seconds_str):
         create_time = datetime.fromtimestamp(float(seconds_str))
-        article_date = datetime.strftime(create_time, '%Y-%m-%d')
+        article_date = datetime.strftime(create_time, "%Y-%m-%d")
         return article_date
 
     def _which_data(self, title: str) -> int:
-        if not self.is_contain_str(title, self.nlp_invaild_keywords) \
-                and self.is_contain_str(title, self.nlp_keywords):
+        if not self.is_contain_str(
+            title, self.nlp_invaild_keywords
+        ) and self.is_contain_str(title, self.nlp_keywords):
             return 0
 
-        if not self.is_contain_str(title, self.others_invalid_keywords) \
-                and self.is_contain_str(title, self.others_keywords):
+        if not self.is_contain_str(
+            title, self.others_invalid_keywords
+        ) and self.is_contain_str(title, self.others_keywords):
             return 1
 
         if self.is_contain_str(title, self.interview_keywords):
@@ -135,26 +140,29 @@ class SpiderArticleWeChat(object):
 
     def read_md_data_split(self, md_path):
         already_data = self.read_txt(md_path)
-        idx = already_data.index('---')
-        first, second = already_data[:idx + 1], already_data[idx + 1:]
+        idx = already_data.index("---")
+        first, second = already_data[: idx + 1], already_data[idx + 1 :]
         return first, second
 
     @staticmethod
     def read_txt(txt_path: str) -> list:
-        with open(txt_path, 'r', encoding='utf-8') as f:
-            data = list(map(lambda x: x.rstrip('\n'), f))
+        with open(txt_path, "r", encoding="utf-8") as f:
+            data = list(map(lambda x: x.rstrip("\n"), f))
         return data
 
     @staticmethod
-    def write_txt(save_path: str, content: list, mode='w'):
+    def write_txt(save_path: str, content: list, mode="w"):
         if isinstance(content, str):
             content = [content]
-        with open(save_path, mode, encoding='utf-8') as f:
+        with open(save_path, mode, encoding="utf-8") as f:
             for value in content:
-                f.write(f'{value}\n')
+                f.write(f"{value}\n")
 
     @staticmethod
-    def is_contain_str(sentence: str, key_words: Union[str, List],) -> bool:
+    def is_contain_str(
+        sentence: str,
+        key_words: Union[str, List],
+    ) -> bool:
         """sentences中是否包含key_words中任意一个"""
         sentence = sentence.lower()
         return any(i.lower() in sentence for i in key_words)
@@ -167,7 +175,7 @@ class SpiderArticleWeChat(object):
 
     @staticmethod
     def read_yaml(yaml_path):
-        with open(yaml_path, 'rb') as f:
+        with open(yaml_path, "rb") as f:
             data = yaml.load(f, Loader=yaml.Loader)
         return data
 
@@ -181,9 +189,13 @@ def main(args):
         print(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cookie', type=str, default='''RK=49MwDvMBSK; ptcz=a23d9b1a2561d2e360381c6184a138f490764fa976bc0544743b1edd3d2ce9e0; ua_id=kzAc4J4XjeTMP6UDAAAAAPsMj9BTC19udOss8TlZJCQ=; wxuin=59623090480645; mm_lang=zh_CN; pgv_pvid=1789084112; pac_uid=0_d10cb03428788; ts_uid=6233800786; __root_domain_v=.weixin.qq.com; _qddaz=QD.750560569830378; o_cookie=1226778264; fqm_pvqid=0b780fa5-8d7f-498d-9d51-2dfc09511b15; tvfe_boss_uuid=e5ba87a04633837f; uuid=5c8321e8d3d730221225d2a6dccf576f; rand_info=CAESILihhmOMo3vmVe2nSGQ9Sz4Fr3NCMrRdgZeUwwHfabW0; slave_bizuin=3874891165; data_bizuin=3874891165; bizuin=3874891165; data_ticket=K+pusiOsCPjSVaJdslo36HnvNdWMV5fMBoBduXtESCLAOzaa3pRj46w0ibogNw0R; slave_sid=MTh2eXF2b3ZLV3JRSjZkN3BZd3BjTmdNVl9CQ2RFTGlVN0xpOFJ6UFRtREZlRGZsS0M1aFBLRzdJX19zVFhDQjd1eEtyN0xjbGk1bzloSTcybHdXX05OMjdqTkxwTUdFekJqX3hDRk9PUl9iREdONFFwMmh5YWVFdmVZYUh1bGdDcWlzWkhqUE1RRlVGUlhl; slave_user=gh_c4775c62f354; xid=adc2a1fd1b5122a2687b4fb505668c91; _clck=3874891165|1|fb5|0; _clsk=1kuotvf|1682689090912|2|1|mp.weixin.qq.com/weheat-agent/payload/record''')
-    parser.add_argument('--token', type=int, default=1956796515)
+    parser.add_argument(
+        "--cookie",
+        type=str,
+        default="""RK=49MwDvMBSK; ptcz=a23d9b1a2561d2e360381c6184a138f490764fa976bc0544743b1edd3d2ce9e0; ua_id=kzAc4J4XjeTMP6UDAAAAAPsMj9BTC19udOss8TlZJCQ=; wxuin=59623090480645; mm_lang=zh_CN; pgv_pvid=1789084112; ts_uid=6233800786; __root_domain_v=.weixin.qq.com; _qddaz=QD.750560569830378; o_cookie=1226778264; fqm_pvqid=0b780fa5-8d7f-498d-9d51-2dfc09511b15; tvfe_boss_uuid=e5ba87a04633837f; pac_uid=1_1226778264; ts_refer=file.daihuo.qq.com/; rewardsn=; wxtokenkey=777; uuid=39f6acbff0e36567e57b212ee0173e18; rand_info=CAESIJzKJ9OVYDeaOu1EzR0chjzbAEDW9Rt5VF/r/f2pc5gr; slave_bizuin=3874891165; data_bizuin=3874891165; bizuin=3874891165; data_ticket=1Q0O6iXjsF4T4f66PTSAfUcnZKs6utij9NnJdLQmr+9NMA5UA2QCPWd8gvX5g/9E; slave_sid=bWZ6V3p6MU5YSjJyMXlLM0xKbEoxa1p0RG9OVEhXd0JZNlJ1VDNTMHpsZ2Z6eWlEQVJocGV2djZQRWpiR0d2eEhVbUJTX0dja0VQcnI2R3dsTERRdjBtWG01ZHZaWWNKZnFsY0JkbjVzZ0cxbkthcXVpcFBIZjE3anNQZ2NuQk5FVExwRUx4ekdsQTZod3l0; slave_user=gh_c4775c62f354; xid=f4cdbc87f7040878e01ed75b9f8a0421; _clck=3874891165|1|fdo|0; _clsk=1wl6i4k|1690549248546|15|1|mp.weixin.qq.com/weheat-agent/payload/record""",
+    )
+    parser.add_argument("--token", type=int, default=1848909564)
     args = parser.parse_args()
     main(args)

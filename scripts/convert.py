ba94424e5db956c63cee71c294ab0b6395db2900
SWHL <liekkaskono@163.com>
2024-04-24 10:05:24 +0800

    Update docs

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,7 +7,24 @@ from pathlib import Path
 import yaml
 
 
-comment_js = '<script src="https://giscus.app/client.js"data-repo="SWHL/AI-Competition-Collections"data-repo-id="MDEwOlJlcG9zaXRvcnkzNjI2NTQ0NDA="data-category="Ideas"data-category-id="DIC_kwDOFZ2q6M4Ce5Hv"data-mapping="title"data-strict="0"data-reactions-enabled="1"data-emit-metadata="0"data-input-position="top"data-theme="preferred_color_scheme"data-lang="zh-CN"data-loading="lazy"crossorigin="anonymous"async></script>'
+comment_js = """
+<script src="https://giscus.app/client.js"
+        data-repo="SWHL/AI-Competition-Collections"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkzNjI2NTQ0NDA="
+        data-category="Ideas"
+        data-category-id="DIC_kwDOFZ2q6M4Ce5Hv"
+        data-mapping="title"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
+"""
 
 
 class ConvertMDToHugo:
@@ -154,7 +171,6 @@ class ConvertMDToHugo:
             " ",
             self.children,
             " ",
-            comment_js
         ]
 
         readme_path = self.root_dir / "README.md"
